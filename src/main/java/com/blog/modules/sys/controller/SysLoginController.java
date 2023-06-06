6de694194a9087d8e3cc03f47af30494ed360799
aibinru <am19136754034@gamil.com>
2023-06-06 23:55:20 +0800

    登录验证码控制类--测试提交

---
+++ src/main/java/com/blog/modules/sys/controller/SysLoginController.java
@@ -0,0 +1,12 @@
+package com.blog.modules.sys.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author:艾宾如
+ * @createDate:2023/6/6
+ * @description:
+ **/
+@Controller
+public class SysLoginController {
+}
