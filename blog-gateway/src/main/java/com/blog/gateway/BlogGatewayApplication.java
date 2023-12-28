9c3437db60d6483cf713d1fc6f5152e4c66f86c4
aibinru <am19136754034@gamil.com>
2023-12-28 18:04:04 +0800

    博客系统-添加网关模块，添加阿里巴巴nacos-注册与发现

--- blog-gateway/src/main/java/com/blog/gateway/BlogGatewayApplication.java
+++ blog-gateway/src/main/java/com/blog/gateway/BlogGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.blog;
+package com.blog.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class BlogAdminApplication {
+public class BlogGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlogAdminApplication.class, args);
+        SpringApplication.run(BlogGatewayApplication.class, args);
     }
 
 }
