bbf657f63cb1e0d96add95853d9542dd305576d4
aibinru <am19136754034@gamil.com>
2024-01-05 15:25:51 +0800

    根据用户名查询

--- blog-admin/src/main/java/com/blog/modules/sys/controller/SysLoginController.java
+++ blog-admin/src/main/java/com/blog/modules/sys/controller/SysLoginController.java
@@ -1,8 +1,13 @@
 package com.blog.modules.sys.controller;
 
 
+import com.blog.modules.sys.entity.SysUserEntity;
+import com.blog.modules.sys.form.SysLoginForm;
 import com.blog.modules.sys.service.SysCaptchaService;
+import com.blog.modules.sys.service.SysUserService;
 import com.blog.modules.sys.service.SysUserTokenService;
+import com.common.utils.R;
+import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,8 +33,8 @@ public class SysLoginController {
 
     @Autowired
     private SysCaptchaService sysCaptchaService;
-//    @Autowired
-//    private SysUserService sysUserService;
+    @Autowired
+    private SysUserService sysUserService;
     @Autowired
     private SysUserTokenService sysUserTokenService;
 
@@ -52,39 +57,39 @@ public class SysLoginController {
         IOUtils.closeQuietly(out);
     }
 
-//    @PostMapping("/login")
-//    public Map<String, Object> login(@RequestBody SysLoginForm form) {
-//        boolean captcha = sysCaptchaService.validate(form.getCaptcha());
-//        if (!captcha) {
-//            return R.error("验证码不正确");
-//        }
-//
-//        //用户信息
-//        SysUserEntity user = sysUserService.queryByUserName(form.getUsername());
-//
-//        //账号不存在，密码错误
-//        if(user == null || !user.getPassword().equals(new Sha256Hash(form.getPassword(), user.getSalt()).toHex())){
-//            return R.error("账号或密码不正确");
-//        }
-//
-//        if(user.getStatus() == 0){
-//            return R.error("账号已被锁定，请联系管理员");
-//        }
-//
-//        //生成token，并保存到数据库
-//        R r = sysUserTokenService.createToken(user.getUserId());
-//        System.out.println("登录成功");
-//        return null;
-//    }
-//
-//    /**
-//     * 退出
-//     */
-//    @PostMapping("/logout")
-//    public R logout() {
-//        sysUserTokenService.logout(1);
-//        return R.ok();
-//    }
+    @PostMapping("/login")
+    public Map<String, Object> login(@RequestBody SysLoginForm form) {
+        boolean captcha = sysCaptchaService.validate(form.getCaptcha());
+        if (!captcha) {
+            return R.error("验证码不正确");
+        }
+
+        //用户信息
+        SysUserEntity user = sysUserService.queryByUserName(form.getUsername());
+
+        //账号不存在，密码错误
+        if(user == null || !user.getPassword().equals(new Sha256Hash(form.getPassword(), user.getSalt()).toHex())){
+            return R.error("账号或密码不正确");
+        }
+
+        if(user.getStatus() == 0){
+            return R.error("账号已被锁定，请联系管理员");
+        }
+
+        //生成token，并保存到数据库
+        R r = sysUserTokenService.createToken(user.getUserId());
+        System.out.println("登录成功");
+        return null;
+    }
+
+    /**
+     * 退出
+     */
+    @PostMapping("/logout")
+    public R logout() {
+        sysUserTokenService.logout(1);
+        return R.ok();
+    }
 
 
 }
